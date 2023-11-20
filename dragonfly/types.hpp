a7b5ef32e90a457cd4ebb67795db7cca59cb9b15
blue <67697646+m256i@users.noreply.github.com>
2023-11-20 14:20:50 +0100

    Update types.hpp

--- dragonfly/types.hpp
+++ dragonfly/types.hpp
@@ -1,17 +1,26 @@
 #ifndef DRAGONFLY_TYPES_HPP
 #define DRAGONFLY_TYPES_HPP
 
+#include <cstdint>
+#include <cstddef>
+
 namespace dfly {
-	using u8 = unsigned char;
-	using u16 = unsigned short;
-	using u32 = unsigned int;
-	using u64 = unsigned long long int;
-	using s8 = signed char;
-	using s16 = signed short;
-	using s32 = signed int;
-	using s64 = signed long long int;
+	using u0 = void;
+
+	using u8 = std::uint8_t;
+	using u16 = std::uint16_t;
+	using u32 = std::uint32_t;
+	using u64 = std::uint64_t;
+	using s8 = std::int8_t;
+	using s16 = std::int16_t;
+	using s32 = std::int32_t;
+	using s64 = std::int64_t;
 	using f32 = float;
 	using f64 = double;
+
+	using usize = std::size_t;
+	using uptr = std::uintptr_t;
+	using ptrdiff = std::ptrdiff_t;
 }
 
 #endif
